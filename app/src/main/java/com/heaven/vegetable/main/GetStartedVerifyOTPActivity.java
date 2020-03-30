c0c70935226f6221f40b05a6146cebf84e03d52f
pradeepjadhav <pradeepjadhav18@gmail.com>
2020-03-30 10:56:20 +0530

    comment

--- app/src/main/java/com/heaven/vegetable/main/GetStartedVerifyOTPActivity.java
+++ app/src/main/java/com/heaven/vegetable/main/GetStartedVerifyOTPActivity.java
@@ -20,6 +20,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskExecutors;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -87,9 +88,9 @@ public class GetStartedVerifyOTPActivity extends AppCompatActivity implements OT
             flagCalledFrom = bundle.getString("CalledFrom");
         }
 
-        mobileNumber = Application.userDetails.getMobile();
         mAuth = FirebaseAuth.getInstance();
         SMSListener.bindListener(this);
+        mobileNumber = Application.userDetails.getMobile();
 
         init();
         events();
